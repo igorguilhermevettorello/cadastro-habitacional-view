cd55de5a26bb6a63547584315c42bc94b0342d32
Igor Vettorello <igor.vettorello@hotmail.com>
2018-04-09 14:13:30 -0300

    alteração de acesso

--- app/globals.js
+++ app/globals.js
@@ -4,7 +4,8 @@ core_1.Injectable();
 var Globals = (function () {
     function Globals() {
         //public url : string = 'http://localhost:3100';
-        this.url = 'https://cadastro-habitacional-server.herokuapp.com';
+        //public url : string = 'https://cadastro-habitacional-server.herokuapp.com';
+        this.url = 'https://habitacao.campobom.rs.gov.br';
     }
     return Globals;
 }());
--- app/globals.ts
+++ app/globals.ts
@@ -4,5 +4,6 @@ Injectable()
 
 export class Globals {
     //public url : string = 'http://localhost:3100';
-    public url : string = 'https://cadastro-habitacional-server.herokuapp.com';
+    //public url : string = 'https://cadastro-habitacional-server.herokuapp.com';
+    public url : string = 'https://habitacao.campobom.rs.gov.br';
 }
