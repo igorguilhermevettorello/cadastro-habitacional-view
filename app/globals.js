a43dddc6321536dcb8e6fb2d5f38d2a645759daa
Igor Vettorello <igor.vettorello@hotmail.com>
2018-04-10 14:26:56 -0300

    ajuste

--- app/globals.js
+++ app/globals.js
@@ -4,8 +4,7 @@ core_1.Injectable();
 var Globals = (function () {
     function Globals() {
         //public url : string = 'http://localhost:3100';
-        //public url : string = 'https://cadastro-habitacional-server.herokuapp.com';
-        this.url = 'https://habitacao.campobom.rs.gov.br';
+        this.url = 'https://cadastro-habitacional-server.herokuapp.com';
     }
     return Globals;
 }());
